8d126d4d847b687982d505cfe5ba311858aad48e
nepthius <anishdshekar@gmail.com>
2025-03-14 13:25:53 -0400

    add rectangle in class function

--- chapter-5/chap5/src/main.rs
+++ chapter-5/chap5/src/main.rs
@@ -5,6 +5,17 @@ struct User{
     sign_in_count: u64
 }
 
+struct Rectangle {
+    width:u32,
+    height:u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width*self.height
+    }
+}
+
 fn main() {
     let user1 = User {
         active: true, 
@@ -17,6 +28,28 @@ fn main() {
     let user2 = build_user(&user1.email, &user1.username);
     println!("{}", user2.username);
     println!("{}", user1.username);
+
+
+
+    let user3  = User{
+        username: String::from("FlameON!"),
+        ..user1
+    };
+    println!("{}", user3.username);
+    println!("{}", user3.email);
+    //user1's username/email does not work anymore
+    println!("{}", user1.active);
+
+    struct flame(i32, i32,i32);
+    let flash = flame(2,3,4);
+
+    let rect1 = Rectangle{
+        width:10,
+        height:10
+    };
+    println!("{}", rect1.area());
+
+
 }
 
 fn build_user(email: &String, username: &String) -> User {
