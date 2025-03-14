ae54f87d3b9b12effa1bf555aa9ca1daa30b9c1e
nepthius <anishdshekar@gmail.com>
2025-03-14 11:17:56 -0400

    Add pointer pass to build_user

--- chapter-5/chap5/src/main.rs
+++ chapter-5/chap5/src/main.rs
@@ -1,11 +1,11 @@
-fn main() {
-    struct User{
-        active: bool,
-        username: String,
-        email: String,
-        sign_in_count: u64
-    }
+struct User{
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
 
+fn main() {
     let user1 = User {
         active: true, 
         username: String::from("Myapple"),
@@ -14,4 +14,16 @@ fn main() {
     };
 
     println!("{}", user1.username);
+    let user2 = build_user(&user1.email, &user1.username);
+    println!("{}", user2.username);
+    println!("{}", user1.username);
+}
+
+fn build_user(email: &String, username: &String) -> User {
+    User{
+        active:true,
+        username: String::from(username),
+        email: String::from(email),
+        sign_in_count: 1
+    }
 }
